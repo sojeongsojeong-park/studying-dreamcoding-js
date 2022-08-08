b1b4e738816162ffb4efa1d96a4438df73debcb7
sojeongsojeong-park <psj90972@naver.com>
2022-08-08 11:48:59 +0900

    javascript ~ generator

--- 9.iteration/4.generator.js
+++ 9.iteration/4.generator.js
@@ -10,13 +10,24 @@
 // },
 //}
 
+//function*  => generator
 function* multipleGenerator() {
-  for (let i = 0; i < 10; i++) {
-    console.log(i);
-    yield i ** 2;
+  try {
+    for (let i = 0; i < 10; i++) {
+      console.log(i);
+      yield i ** 2;
+    }
+  } catch (e) {
+    console.log(e);
   }
 }
 
 const multiple = multipleGenerator();
 let next = multiple.next();
+console.log(next.value, next.done);
+
+//multiple.return();
+multiple.throw("Error!");
+
+next = multiple.next();
 console.log(next.value, next.done);
