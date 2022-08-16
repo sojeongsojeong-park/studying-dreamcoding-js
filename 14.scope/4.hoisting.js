5fc5a7a988326bda5e3383b411de2cedf9f611dc
sojeongsojeong-park <psj90972@naver.com>
2022-08-16 20:02:08 +0900

    javascript ~execution context

---
+++ 14.scope/4.hoisting.js
@@ -0,0 +1,12 @@
+//실행 컨텍스트 Execution Context
+//코드의 실행 순서와 스코프를 기억
+
+//호이스팅(Hoisting)
+//자바스크립트 엔진(번역기, Interpreter)이 코드를 실행하기 전,
+//변수, 함수, 클래스의 선언문을 끌어 올리는 것을 말함.
+
+function print() {
+  console.log("Hello");
+}
+
+print();
